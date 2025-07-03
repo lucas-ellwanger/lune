0971af25d2dc4851e432357310defe19010a8612
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-07-03 15:37:02 -0300

    04: background jobs

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -16,6 +16,7 @@ const config = {
     "^@/config/(.*)$",
     "^@/lib/(.*)$",
     "^@/hooks/(.*)$",
+    "^@/inngest/(.*)$",
     "^@/trpc/(.*)$",
     "^@/components/ui/(.*)$",
     "^@/components/(.*)$",
